1f0fea58b0f3b407e072036497e4a9fcb37ffb38
Haneul <haneul20020507@gmail.com>
2025-07-07 21:15:15 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ darkTheme = createTheme(darkTheme, {
           width: 22,
           height: 22,
           // OFFの時のつまみの色もテーマのprimaryカラーを参照
-          color: '#205020',
+          color: darkTheme.palette.primary.main,
         },
         track: {
           borderRadius: 26 / 2,
